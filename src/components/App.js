d0e7b9bfd471de62654f459a0729b3894004f9b0
Ben Kahle <ben@kahle.org>
2017-05-09 22:37:24 -0700

    move firebase into app to enable exporting

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,23 @@
 import React, { Component } from 'react'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/storage'
+import 'firebase/database'
+
 import logo from '../logo.svg'
 import '../styles/App.scss'
+
+var config = {
+  apiKey: 'AIzaSyD3xxNtRdqgqbYw2WUlmswKT3eEZbF6Q1M',
+  authDomain: 'blanks-5a110.firebaseapp.com',
+  databaseURL: 'https://blanks-5a110.firebaseio.com',
+  projectId: 'blanks-5a110',
+  storageBucket: 'blanks-5a110.appspot.com',
+  messagingSenderId: '801475889815'
+}
+firebase.initializeApp(config)
+
+export { firebase }
 
 class App extends Component {
   render () {
--- src/index.js
+++ src/index.js
@@ -3,22 +3,6 @@ import ReactDOM from 'react-dom'
 import App from './components/App'
 import './styles/index.scss'
 
-import firebase from 'firebase/app'
-import 'firebase/auth'
-import 'firebase/storage'
-import 'firebase/database'
-
-var config = {
-  apiKey: 'AIzaSyD3xxNtRdqgqbYw2WUlmswKT3eEZbF6Q1M',
-  authDomain: 'blanks-5a110.firebaseapp.com',
-  databaseURL: 'https://blanks-5a110.firebaseio.com',
-  projectId: 'blanks-5a110',
-  storageBucket: 'blanks-5a110.appspot.com',
-  messagingSenderId: '801475889815'
-}
-firebase.initializeApp(config)
-console.log(firebase)
-
 ReactDOM.render(
   <App />,
   document.getElementById('root')
